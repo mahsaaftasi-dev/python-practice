e7ca5b61c6ce902f8d1c48f8f26852cad3bfcf05
mahsaaftasi-dev <mahsa.aftasi@gmail.com>
2025-10-26 16:22:02 +0330

    numpy2.py

---
+++ Numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(a + b)
+print(a * b)
+print(a ** 2)
+print("Shape:", a.shape)
+print("Dimension:", b.ndim)
+print("Data type:", a.dtype)
---
+++ Numpy2.py
@@ -0,0 +1,7 @@
+import numpy as np
+A= np.arange(12).reshape(3,4)
+print(A)
+B = np.arange(10, 20).reshape(2, 5)
+print("\nMatrix B:\n", B)
+print("\nShape of A:", A.shape)
+print("Dimensions of A:", A.ndim)
